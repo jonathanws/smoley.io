cd53124111554469c5b151c454de3d5fda740029
Jon Smoley <jon.smoley@gmail.com>
2020-11-05 16:03:20 -0700

    Added icons

---
+++ src/images/icons/MailIcon.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const HomeIcon = () => (
+	<svg
+		xmlns="http://www.w3.org/2000/svg"
+		className="icon icon-tabler icon-tabler-mail"
+		width="36"
+		height="36"
+		viewBox="0 0 24 24"
+		stroke-width="2.5"
+		stroke="#607D8B"
+		fill="none"
+		stroke-linecap="round"
+		stroke-linejoin="round"
+	>
+		<path stroke="none" d="M0 0h24v24H0z" fill="none" />
+		<rect x="3" y="5" width="18" height="14" rx="2" />
+		<polyline points="3 7 12 13 21 7" />
+	</svg>
+)
+
+export default HomeIcon
---
+++ src/images/icons/README.md
@@ -0,0 +1,7 @@
+# Tabler Icons
+
+These files all come from [Tabler Icons](https://tablericons.com/)
+
+I set the size as **36px**, and stroke as **2.5px**
+
+Color should be irrelevant since it will be set with css, so I just put it as white
